210b425a99c5ccf290ffee121c93611870ddbff7
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-14 11:26:23 -0400

    created API fetch for ghibli films. found path in object to access each movie title.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Trigger");
+let mainSection = document.querySelector("#main-section");
+fetch("https://ghibliapi.herokuapp.com/films")
+    .then((res)=> res.json())
+    .then((data)=>{
+        console.log(data);
+        console.log(data[0].title); // first movie only
+
+        let movieData = data;
+
+        for (let movie of movieData) {
+            console.log(movie.title);
+        }
+    });
