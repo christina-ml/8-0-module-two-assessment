3551a06a978372fbfcd0413bd664eebad9091b6b
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-14 11:35:05 -0400

    added each movie as an option in the select box. removed default option from html

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ fetch("https://ghibliapi.herokuapp.com/films")
     .then((res)=> res.json())
     .then((data)=>{
         console.log(data);
-        console.log(data[0].title); // first movie only
-
         let movieData = data;
 
+        /* Select where movie title will go */
+        let movieTitlesSelection = document.querySelector("#movie-titles-selection")
+
+        /* Add each movie as an option element to the `select` section */
         for (let movie of movieData) {
             console.log(movie.title);
+            let movieOption = document.createElement("option");
+            movieOption.textContent = movie.title;
+            movieOption.value = movie.title;
+            movieTitlesSelection.append(movieOption);
         }
-    });
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
