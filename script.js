04ec3da4501d1a60e4efc1646763bf55e9900f80
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-14 11:51:03 -0400

    selected dropdown list and added event listener on change. movie title appears using innerHTML when movie is selected.

--- script.js
+++ script.js
@@ -10,12 +10,32 @@ fetch("https://ghibliapi.herokuapp.com/films")
 
         /* Add each movie as an option element to the `select` section */
         for (let movie of movieData) {
-            console.log(movie.title);
+            // console.log(movie.title);
             let movieOption = document.createElement("option");
             movieOption.textContent = movie.title;
             movieOption.value = movie.title;
             movieTitlesSelection.append(movieOption);
         }
+
+        /* Select dropdown list */
+        let dropDownMovies = document.querySelector("#movie-titles-selection");
+        /* Select `display-info` section where movie title will go */
+        let displayInfo = document.querySelector("#display-info");
+
+        /* Event Listener on dropdown list */
+        dropDownMovies.addEventListener("change", (e) => {
+            for (let movie of movieData) {
+                if (dropDownMovies.value === movie.title) {
+                    console.log(movie.title);
+                    displayInfo.innerHTML = `<h3>${movie.title}</h3>
+                    <p id="release-year"></p>
+                    <p id="movie-description"></p>`;
+                }
+            }
+        });
+
+
+
     })
     .catch((err)=>{
         console.log(err);
