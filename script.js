040b927ef9494c0d8919bbeb243578ffeba68d6f
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-15 12:18:44 -0400

    Step 2 complete - populate dropdown with movie titles from API data

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ fetch("https://ghibliapi.herokuapp.com/films")
     })
     .then((movies)=>{
         console.log(movies);
+        // STEP 2 ---------  POPULATE DROPDOWN WITH MOVIE TITLES FROM API DATA --------- 
+        const dropdown = document.querySelector("#dropdown select");
+        for (let movie of movies){
+            const option = document.createElement("option");
+            option.textContent = movie.title;
+            option.setAttribute("value", movie.title);
+            dropdown.append(option);
+        }
     })
 
-// STEP 2 ---------  POPULATE DROPDOWN WITH MOVIE TITLES FROM API DATA --------- 
 // STEP 3 ---------  DISPLAY SELECTED MOVIE'S DATA --------- 
 // STEP 4 --------- ADD FUNCTIONALITY TO FORM TO LEAVE MOVIE REVIEW --------- 
